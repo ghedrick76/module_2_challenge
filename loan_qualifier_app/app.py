71fa60bf2cafe89b15add87960b75df0f98e71af
ghedrick76 <graysonh76@icloud.com>
2022-04-02 17:21:26 -0400

    moved save_csv function to utils folder

--- loan_qualifier_app/app.py
+++ loan_qualifier_app/app.py
@@ -12,6 +12,8 @@ import questionary
 import csv
 from pathlib import Path
 
+from qualifier.utils.save_csv import save_csv
+
 from qualifier.utils.fileio import load_csv
 
 from qualifier.utils.calculators import (
@@ -103,22 +105,6 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
     return bank_data_filtered
 
 
-
-
-
-def save_csv(qualifying_loans):
-    header = ["Qualifying Loans"]
-    """Saves the qualifying loans to a CSV file.
-
-    Args:
-        qualifying_loans (list of lists): The qualifying bank loans.
-    """
-    # @TODO: Complete the usability dialog for savings the CSV Files.
-    # YOUR CODE HERE!
-    with open('/Users/grayson/Documents/Challenges/module_2_challenge/loan_qualifier_app/data/qualified_loans.csv', 'w') as f:
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(header)
-        csv_writer.writerow(qualifying_loans)
 
 
 def run():
