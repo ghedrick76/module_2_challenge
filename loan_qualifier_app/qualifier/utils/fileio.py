bfa8e69256a8ea837bdc608ea7d131bf64267c46
ghedrick76 <graysonh76@icloud.com>
2022-04-03 22:12:09 -0400

    Moved save_csv function to fileio.py to minimize clutter, and also fixed the code within the loop in save_csv function

--- loan_qualifier_app/qualifier/utils/fileio.py
+++ loan_qualifier_app/qualifier/utils/fileio.py
@@ -28,3 +28,22 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+# function for creating a CSV file of the qualifying loans, using the list
+# of qualifying loans as an argument.
+def save_csv(qualifying_loans):
+    header = ["Lender", "Max Loan Amount", "Max LTV", "Max DTI", "Min Credit Score", "Interest Rate"]
+    """Saves the qualifying loans to a CSV file.
+
+    Args:
+        qualifying_loans (list of lists): The qualifying bank loans.
+    """
+    # @TODO: Complete the usability dialog for savings the CSV Files.
+    # YOUR CODE HERE!
+    # chooses the file to write the new information to
+    with open('/Users/grayson/Documents/Challenges/module_2_challenge/loan_qualifier_app/data/qualified_loans.csv', 'w') as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(header)
+        for loan in qualifying_loans:
+            csv_writer.writerow(loan)
+       
